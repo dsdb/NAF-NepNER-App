a7364d922a4d42896fd9b271cb6694c8ce51a8b5
dsdb <40564664+dsdb@users.noreply.github.com>
2023-10-06 14:05:17 +0530

    Add files via upload
    version upgrader

--- app/nepali_ner_ftm_app.py
+++ app/nepali_ner_ftm_app.py
@@ -4,10 +4,18 @@ import torch
 
 # NER model and tokenizer
 model_name = "debabrata-ai/NepNER"
-model = AutoModelForTokenClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained("debabrata-ai/NepNER")
+@st.cache_data
+def get_NER_model():    
+    model = AutoModelForTokenClassification.from_pretrained(model_name)
+    return model
 
-ner_labels = model.config.id2label
+@st.cache_data
+def get_NER_tokenizer(): 
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return tokenizer
+
+#ner_labels = model.config.id2label
+ner_labels = get_NER_model().config.id2label
 
 # title
 st.title("Nepali NER Prediction App ")
@@ -17,11 +25,14 @@ user_input = st.text_area("Enter a sentence or paragraph:")
 
 if st.button("Predict NER"):
     if user_input:
-        tokens = tokenizer.tokenize(tokenizer.decode(tokenizer.encode(user_input)))
-        inputs = tokenizer.encode(user_input, return_tensors="pt")
+       
+        tokens = get_NER_tokenizer().tokenize(get_NER_tokenizer().decode(get_NER_tokenizer().encode(user_input)))
+        inputs = get_NER_tokenizer().encode(user_input, return_tensors="pt")
+        
 
         with torch.no_grad():
-            outputs = model(inputs).logits
+           
+            outputs = get_NER_model()(inputs).logits
 
         predictions = torch.argmax(outputs, dim=2).squeeze().tolist()
         ner_tags = [ner_labels[label_id] for label_id in predictions]
